178fdf575efd5b46ae7a56a68ffb5e01f39bf5c0
ahmedsabry10 <ahmaasabry22@gmail.com>
2022-04-24 00:39:31 +0200

    new

--- lib/View/screens/SideMenu_Screens/instructions_screen.dart
+++ lib/View/screens/SideMenu_Screens/instructions_screen.dart
@@ -35,26 +35,34 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
+
+                  //first & second
                   Text(
                     " First, we need to warn that food or clothing should be delivered to those who deserve it, the needy and the poor, so that we may receive the reward for all. \n \n Second, we will explain some of the instructions that must be followed with food of all kinds or clothing, whether you are a donor or an applicant for food or clothing:",
                     textDirection: TextDirection.ltr,
+                    style: TextStyle( fontSize: 17),
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
                     height: 15,
                   ),
+
+
+                  //uncooked
                   Text(
                     " * UnCooked ",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
                     height: 10,
                   ),
+
+                  //Meat and fish and chicken
                   Text(
-                    "- Meat and fish and chicken",
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                    ">> Meat and fish and chicken",
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -63,6 +71,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "1- Products should have a distinctive appearance, color and aroma",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -71,6 +80,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "2- Check the expiration date",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -80,6 +90,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   Text(
                     "3-Products should be stored in the fridge or freezer according to the manufacturer's or pressing/butcher's instructions",
                     textDirection: TextDirection.ltr,
+                    style: TextStyle( fontSize: 15),
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
@@ -87,6 +98,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "4- To be away from any source of pollution ",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -95,15 +107,19 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "5- Separate different types of meat, poultry and fish from each other and from other foods ",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
                     height: 15,
                   ),
+
+
+                  //Vegetables and Fruits
                   Text(
-                    "- Vegetables and Fruits",
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                    ">> Vegetables and Fruits",
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -112,7 +128,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "1- Vegetables should be washed well in cold water and then dried with a paper towel.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -121,7 +137,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "2- Put the vegetables in plastic bags and close them tightly after they are completely deflated.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -130,7 +146,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "3- It is not preferable to keep vegetables and fruits together in one place in the refrigerator",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -139,7 +155,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "4- Leave some space in the bags with holes in them so that the fruit can breathe",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -148,16 +164,18 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "5- Keeping fruits and Vegetables in the fridge",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
                     height: 15,
                   ),
+
+                  //Legumes and grains
                   Text(
-                    "- Legumes and grains",
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+                    ">> Legumes and grains",
+                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -166,7 +184,7 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "1- Use airtight glass containers for preservation",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -175,16 +193,20 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   ),
                   Text(
                     "2-Ensure that the container is completely clean and dry of water or liquids to avoid moisture during preservation, which damages the legumes",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
                   SizedBox(
                     height: 15,
                   ),
+
+
+
+                  //Cooked
                   Text(
                     " * Cooked ",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -192,8 +214,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 10,
                   ),
                   Text(
-                    "- Make sure to put food in the fridge before putting it in the freezer.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "1- Make sure to put food in the fridge before putting it in the freezer.",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -201,8 +223,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- Food quality should be good and in good condition",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "2- Food quality should be good and in good condition",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -210,8 +232,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- Make sure to use plastic bags designated for freezing and not copper or cooking utensils.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "3- Make sure to use plastic bags designated for freezing and not copper or cooking utensils.",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -219,8 +241,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- Do not leave food out of the refrigerator for more than two hours.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "4- Do not leave food out of the refrigerator for more than two hours.",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -228,8 +250,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- Put the date of release on the bags.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "5- Put the date of release on the bags.",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -237,8 +259,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 4,
                   ),
                   Text(
-                    " - When you take food out of the freezer, do not thaw it at room temperature, but rather put it in the refrigerator.",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "6- When you take food out of the freezer, do not thaw it at room temperature, but rather put it in the refrigerator.",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -248,9 +270,12 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                   SizedBox(
                     height: 15,
                   ),
+
+
+                  //Clothes
                   Text(
-                    " * Clothes:  ",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    " * Clothes ",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -258,8 +283,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 10,
                   ),
                   Text(
-                    "- Clothes must be washed and clean",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "1- Clothes must be washed and clean",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -267,8 +292,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- It must be ironed",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "2- It must be ironed",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
@@ -276,8 +301,8 @@ class _InstructionsScreenState extends State<InstructionsScreen> {
                     height: 7,
                   ),
                   Text(
-                    "- Packing in plastic bags",
-                    style: TextStyle(fontWeight: FontWeight.w400),
+                    "3- Packing in plastic bags",
+                    style: TextStyle( fontSize: 15),
                     textDirection: TextDirection.ltr,
                     textAlign: TextAlign.start,
                   ),
